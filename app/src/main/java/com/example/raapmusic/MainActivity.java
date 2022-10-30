f6b96d6f59824af01f2ad98833f93779514e4dad
rioaryan123 <rioaryan123@gmail.com>
2022-10-30 21:44:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/raapmusic/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.raapmusic;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.icu.text.CaseMap;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initviewpager();
+    }
+
+    private void initviewpager() {
+        ViewPager Viewpager = findViewById(R.id.viewpager);
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new SongsFragment(), "Songs");
+        viewPagerAdapter.addFragments(new AlbumFragment(), "Album");
+        Viewpager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(Viewpager);
+
+    }
+    public static class ViewPagerAdapter extends FragmentPagerAdapter{
+        private ArrayList<Fragment>fragments;
+        private ArrayList<String>titles;
+        public ViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+            this.fragments=new ArrayList<>();
+            this.titles=new ArrayList<>();
+
+        }
+
+        void addFragments(Fragment fragment, String title)
+        {
+
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+    }
+}

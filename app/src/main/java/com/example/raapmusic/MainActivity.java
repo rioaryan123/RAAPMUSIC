1999340b591386179d03e725f352e0470000475c
indraTI21C4 <indrabalitha201@gmail.com>
2022-11-21 10:06:52 +0700

    input 1

--- app/src/main/java/com/example/raapmusic/MainActivity.java
+++ app/src/main/java/com/example/raapmusic/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.raapmusic;
 
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.icu.text.CaseMap;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -16,11 +23,43 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int REQUEST_CODE=1;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        permission();
         initviewpager();
+    }
+
+    private void permission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        != PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}
+            , REQUEST_CODE);
+        }
+        else
+        {
+            Toast.makeText(this, "permision granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public final void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if( requestCode == REQUEST_CODE)
+        {
+            if (grantResults[0] == PERMISSION_GRANTED)
+            {
+                //do ;
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                , REQUEST_CODE);
+            }
+        }
     }
 
     private void initviewpager() {
